961e8fca700f8c1447d9bd08ec42c2f0377e0a2c
Andrey Lavryonov <kedr_club@mail.ru>
2023-03-10 21:52:20 +0300

    feat: adds cards and modal components

---
+++ src/components/Cards/Cards.tsx
@@ -0,0 +1,52 @@
+import React, { ReactElement } from 'react';
+import styles from './Cards.module.css';
+import { setCurrentCard, setIsModal } from '../../redux/slice';
+import { useAppDispatch } from '../../redux/hooks';
+import { ResultsType } from '../../types/types';
+
+const Cards: React.FC<{ card: ResultsType }> = ({ card }): ReactElement => {
+  const dispatch = useAppDispatch();
+
+  const showModal = (event: { preventDefault: () => void }): void => {
+    event.preventDefault();
+    dispatch(setCurrentCard(card));
+    dispatch(setIsModal(true));
+  };
+
+  return (
+    <li className={styles.cards__item} onClick={showModal}>
+      <h3 className={styles.cards__item_title}>{card.name}</h3>
+      <ul className={styles.heightMass__list}>
+        <li className={styles.heightMass__item}>
+          <span className={styles.heightMass__item_ring}>{card.height}</span>
+          <span className={styles.heightMass__item_title}>height</span>
+        </li>
+        <li className={styles.heightMass__item}>
+          <span className={styles.heightMass__item_ring}>{card.mass}</span>
+          <span className={styles.heightMass__item_title}>mass</span>
+        </li>
+      </ul>
+      <ul className={styles.genderBirth__list}>
+        {card.gender !== 'n/a' && (
+          <li
+            className={styles.genderBirth__item}
+            style={
+              card.gender === 'male'
+                ? { backgroundColor: '#73d677' }
+                : card.gender === 'female'
+                ? { backgroundColor: '#c956ff' }
+                : { backgroundColor: '#f5db13' }
+            }
+          >
+            {card.gender}
+          </li>
+        )}
+        {card.birth_year !== 'unknown' && (
+          <li className={styles.genderBirth__item}>{card.birth_year}</li>
+        )}
+      </ul>
+    </li>
+  );
+};
+
+export default Cards;
---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,74 @@
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import styles from './Modal.module.css';
+import alien from '../../assets/images/alien.png';
+import male from '../../assets/images/male.png';
+import female from '../../assets/images/female.png';
+import { setIsModal } from '../../redux/slice';
+
+const Modal: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const state = useAppSelector((state) => state);
+
+  const close = () => dispatch(setIsModal(false));
+
+  return (
+    <section className={styles.modal__container}>
+      <div className={styles.modal__container_opacity} onClick={close}></div>
+      <div className={styles.modal__card}>
+        <button className={styles.modal__close} onClick={close}></button>
+        <div className={styles.modal__card_content}>
+          <div
+            className={styles.modal__left}
+            style={
+              state.currentCard.gender === 'male'
+                ? { backgroundImage: `url(${male})` }
+                : state.currentCard.gender === 'female'
+                ? { backgroundImage: `url(${female})` }
+                : { backgroundImage: `url(${alien})` }
+            }
+          >
+            <ul className={styles.genderBirth__list}>
+              {state.currentCard.gender !== 'n/a' && (
+                <li
+                  className={styles.genderBirth__item}
+                  style={
+                    state.currentCard.gender === 'male'
+                      ? { backgroundColor: '#73d677' }
+                      : state.currentCard.gender === 'female'
+                      ? { backgroundColor: '#c956ff' }
+                      : { backgroundColor: '#f5db13' }
+                  }
+                >
+                  {state.currentCard.gender}
+                </li>
+              )}
+              {state.currentCard.birth_year !== 'unknown' && (
+                <li className={styles.genderBirth__item}>{state.currentCard.birth_year}</li>
+              )}
+            </ul>
+          </div>
+          <div className={styles.modal__right}>
+            <h2 className={styles.modal__title}>{state.currentCard.name}</h2>
+            <ul className={styles.modal__list}>
+              <li>eye color: {state.currentCard.eye_color}</li>
+              <li>skin color: {state.currentCard.skin_color}</li>
+              <li>hair color: {state.currentCard.hair_color}</li>
+            </ul>
+            <ul className={styles.heightMass__list}>
+              <li className={styles.heightMass__item}>
+                <span className={styles.heightMass__item_ring}>{state.currentCard.height}</span>
+                <span className={styles.heightMass__item_title}>height</span>
+              </li>
+              <li className={styles.heightMass__item}>
+                <span className={styles.heightMass__item_ring}>{state.currentCard.mass}</span>
+                <span className={styles.heightMass__item_title}>mass</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Modal;
